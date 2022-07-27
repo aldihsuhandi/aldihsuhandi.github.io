65381baef55a82ea6b57d46559b02fa4951a74c2
aldih suhandi <aldihsuhandi697@gmail.com>
2022-07-27 08:09:37 +0700

    progress

--- js/highlight.js
+++ js/highlight.js
@@ -5,39 +5,48 @@ class Section {
         this.nav = "#" + name + "Nav";
         this.object = $(this.id);
         this.height = 0;
+        this.underline = false;
     }
 }
 
-
 function hightlightNav(sections, currHeight) {
-    sections.forEach(section => {
+    sections.forEach((section) => {
         var currId = section.id;
         var currNav = section.nav;
         var objectHeight = section.object.height();
-        if (section.height <= currHeight && section.height + objectHeight > currHeight) {
-            $(currNav).css("text-decoration", "underline");
-            sections.forEach(e => {
-                if (currId != e.id) {
+        if (
+            section.height <= currHeight &&
+            section.height + objectHeight > currHeight
+        ) {
+            if (section.underline == false) {
+                $(currNav).fadeIn(1000, function () {
+                    $(currNav).css("text-decoration", "underline");
+                });
+                section.underline = true;
+            }
+
+            sections.forEach((e) => {
+                if (currId != e.id && e.underline == true) {
                     var nav = e.nav;
                     $(nav).css("text-decoration", "none");
+                    e.underline = false;
                 }
             });
         }
     });
 }
 
-function build() 
-{
+function build() {
     var sections = [];
     var totalHeight = 0;
-    $("section").each(function() {
-        var id = $(this).attr('id');
+    $("section").each(function () {
+        var id = $(this).attr("id");
         var section = new Section(id);
 
-        if(id != "home") {
+        if (id != "home") {
             section.height = totalHeight;
-            sections.push(section)
         }
+        sections.push(section);
 
         totalHeight += section.object.height();
     });
@@ -46,14 +55,14 @@ function build()
     return sections;
 }
 
-$(window).ready(function() {
+$(window).ready(function () {
     var sections = build();
 
-    sections.forEach(element => {
+    sections.forEach((element) => {
         console.log(element.height);
     });
 
-    $(document).on('scroll', function() {
+    $(document).on("scroll", function () {
         var currHeight = $(window).scrollTop();
         hightlightNav(sections, currHeight);
     });
