9330a8befb90e65440fe1360aeac3350f185b35a
aldihsuhandi <aldihsuhandi697@gmail.com>
2024-11-03 09:13:16 +0700

    update portfolio, to include latest project

--- js/link.js
+++ js/link.js
@@ -1,6 +1,8 @@
 $(window).ready(function() {
     var linkedin = $("#linkedin");
     var github = $("#github");
+    var gitlab = $("#gitlab");
+    var getsuyoubi_id = $("#getsuyoubi-monday-id");
 
     linkedin.click(function() {
         self.open("https://www.linkedin.com/in/aldihsuhandi/");
@@ -8,5 +10,14 @@ $(window).ready(function() {
 
     github.click(function() {
         self.open("https://github.com/aldihsuhandi");
+    });
+
+    gitlab.click(function() {
+        self.open("https://gitlab.com/Getsuyoubi")
+    })
+
+
+    getsuyoubi_id.click(function() {
+        self.open("https://gitlab.com/getsuyoubi1");
     });
 });
